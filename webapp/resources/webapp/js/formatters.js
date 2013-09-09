59b533d7cce6a9dfd77053b4906c46fb85315789
Joel Richardson <joel.richardson@jax.org>
2013-09-09 08:59:53 -0400

    Using new tables in report pages.

--- webapp/resources/webapp/js/formatters.js
+++ webapp/resources/webapp/js/formatters.js
@@ -1,5 +1,11 @@
 (jQuery(function() { // run when the page has loaded.
 
+  /*
+  * Mapping from a database prefix (lowercase) to a record containing the 
+  * info for creating id-based links to that database. The info includes a
+  * URL template and a flag indicating whether the prefix should be stripped
+  * (true) or preserved (false) when forming the link.
+  */
   var urlTemplates = {
       'chebi' : {
 	  stripPrefix : true,
@@ -70,30 +76,63 @@
 	  url : 'http://www.uniprot.org/keywords/@@@@'
           },
       };
-  var formatUrl = function(text){
-    var parts = text.split(":",2);
+
+  /*
+  * Given an id like "MGI:123456" or "OMIM:76543", returns a link to the corresponding page at
+  * the corresponding resource. If the database (as indicated by the prefix part) is unknown, returns
+  * the id unchanged. Otherwise, returns an html <a> tag.
+  */
+  var formatUrl = function(id){
+    var parts = id.split(":",2);
     var e = urlTemplates[parts[0].toLowerCase()];
-    if(!e) return text;
-    var lt = e.stripPrefix ? parts[1] : text;
-    var url= e.url.replace(/@@@@/, lt);
-    return '<i class="icon-globe"></i> <a href="'+url+'" target="_blank" >'+text+'</a>';
+    if(!e) return id;
+    var lt = e.stripPrefix ? parts[1] : id; // link text
+    var url= e.url.replace(/@@@@/, lt);	    // href
+    return '<i class="icon-globe"></i> <a href="'+url+'" target="_blank" >'+id+'</a>';
   };
+
+  /*
+  * Given an OntologyAnnotationEvidence object, format the withText field with links.
+  */
   var formatWithText = function(imObj){ 
     var t = imObj.get('withText');
     if(!t) return null;
     return t.split("|").map(formatUrl).join("<br/>");
   };
 
-  if (intermine) {   // but only if there is something to do.
-    //intermine.setOptions({CellPreviewTrigger: 'hover'});
-    intermine.setOptions({
-      'Location.start': true,
-      'Location.end': true,
-    }, 'intermine.results.formatsets.genomic');
+  var abbreviate = function(val, len){
+    return val.substr(0,len) + (val.length>len ? "...":"");
+  };
 
-    intermine.scope('intermine.results.formatters', 
-        {OntologyAnnotationEvidence: formatWithText});
+  if (intermine) { 
+    //intermine.setOptions({CellPreviewTrigger: 'hover'});
+
+    delete intermine.results.formatters.Publication;
+    delete intermine.results.formatsets.genomic["Publication.title"];
+
+    intermine.setOptions({
+        'Location.start': true,
+        'Location.end': true,
+	'Location.strand' : true
+      }, 
+      'intermine.results.formatsets.genomic');
+
+    // standard Location formatter doesn't include the strand. Here's a shim to make it.
+    var lf = intermine.results.formatters.Location;
+    var lfnew = function(o){
+        var s = o.get('strand');
+        return lf.call(this,o) + (s>0?' +':s<0?' -':'');
+        };
+    lfnew.replaces = lf.replaces;
+    lfnew.merge = lf.merge;
+    lfnew.replaces.push('strand');
+    intermine.results.formatters.Location = lfnew;
+
     intermine.scope('intermine.results.formatsets.genomic', 
-        {'OntologyAnnotationEvidence.withText': true});	
+        {
+	'OntologyAnnotationEvidence.withText' : formatWithText ,
+        'Publication.title': function(o){return abbreviate(o.get("title"),35);}, 
+        'Publication.citation':function(o){return abbreviate(o.get("citation"),35);}
+        });	
   }
 }));
