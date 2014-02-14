330400b039f645c4e6c6d68236ee4882767ced17
Howie Motenko <howie.motenko@jax.org>
2014-02-14 13:26:40 -0500

    seperate etl from full_build

--- admin/buildscripts/buildEtl.sh
+++ admin/buildscripts/buildEtl.sh
@@ -2,6 +2,8 @@
 
 export LD_LIBRARY_PATH=/usr/local/pgsql/lib
 cd ~/etl_build/etl/bin
+git pull
+e=$?; if [ $e -ne 0 ]; then exit $e; fi
 python ./refresh.py
 e=$?; if [ $e -ne 0 ]; then exit $e; fi
 python ./setVersionProperty.py
