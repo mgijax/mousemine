314143a2d8daa7a38925c6a8752915a78e9002c4
Howie Motenko <howie.motenko@jax.org>
2014-07-22 10:59:36 -0400

    clearify text and add error exit code

--- admin/buildscripts/startTomcat.sh
+++ admin/buildscripts/startTomcat.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
-RESULT=`netstat -na | grep 8080 | grep LISTEN | wc -l`
-if [ "$RESULT" = 0 ] 
+IS_RUNNING=`netstat -na | grep 8080 | grep LISTEN | wc -l`
+if [ "$IS_RUNNING" = 0 ] 
 then
 # Start tomcat in logs directory (to ensure intermine.log saved in the logs directory)
 	cd /usr/local/tomcat/logs
 	export CATALINA_OPTS="-Xms2048m -Xmx2048m"
 	export JAVA_OPTS="-Djava.awt.headless=true" 
 	sudo /usr/local/tomcat/bin/startup.sh
-elif [ "$RESULT" != 0 ] 
+elif [ "$IS_RUNNING" != 0 ] 
 then
-	echo "TOMCAT is running."
+	echo "TOMCAT is ready running."
+	exit 99
 fi
